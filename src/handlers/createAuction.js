5f4d45f9ce2ecdf4c34b1d0fb76a597abcc77969
Rahul <rahul.sarma@borgos.in>
2022-03-17 14:04:53 +0530

    done upto post acutionsTable data

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,32 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient(); //allows us to interaction with dynamodb table
+
+async function createAuction(event, context) {
+
+  const { title } = JSON.parse(event.body)
+  const now = new Date()
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  }
+
+  // insert data in dynamoDB table
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction
+  }).promise()
+
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
