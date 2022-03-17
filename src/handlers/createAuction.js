547b9ff310145b2cadd82bf7935247a62a9cb553
Rahul <rahul.sarma@borgos.in>
2022-03-17 17:05:50 +0530

    v27- Optimize - 2

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -17,7 +17,7 @@ async function createAuction(event, context) {
 
   // insert data in dynamoDB table
   await dynamodb.put({
-    TableName: 'AuctionsTable',
+    TableName: process.env.AUCTIONS_TABLE_NAME,
     Item: auction
   }).promise()
 
