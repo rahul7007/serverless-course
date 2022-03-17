f10f6f517cdcdcb0afe2c5632b91d5bec6133f8d
Rahul <rahul.sarma@borgos.in>
2022-03-17 18:24:16 +0530

    middleware added

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,16 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient(); //allows us to interaction with dynamodb table
 
 async function createAuction(event, context) {
 
-  const { title } = JSON.parse(event.body)
+  const { title } = event.body
   const now = new Date()
 
   const auction = {
@@ -16,10 +21,16 @@ async function createAuction(event, context) {
   }
 
   // insert data in dynamoDB table
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction
-  }).promise()
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise()
+  } catch (err) {
+    console.error(err)
+    throw new createError.InternalServerError(error)
+  }
+
 
 
   return {
@@ -28,5 +39,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler())
 
